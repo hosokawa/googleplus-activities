350362034e2adda58c3343e5fe5bcfabf5fb78bf
HOSOKAWA Tetsuichi <hosokawa@ec-giken.com>
2014-03-07 21:39:32 +0900

    設定ファイルの持ち方などリファクタリング

--- config/config.php
+++ config/config.php
@@ -1,3 +1,3 @@
 <?php
-const SCRIPTS = 'https://code.jquery.com/jquery-2.1.0.min.js';
-const STYLES ='/styles/html5-doctor-reset-stylesheet.css,/styles/main.css';
+const SCRIPTS = 'https://code.jquery.com/jquery-2.1.0.min.js,/js/google-plus.js';
+const STYLES = '/styles/html5-doctor-reset-stylesheet.css,/styles/main.css';
