56ba368a22757d232c1677418a02e3846ebb950f
HOSOKAWA Tetsuichi <hosokawa@ec-giken.com>
2014-03-07 14:30:44 +0900

    設定ファイルの配置を変更。

--- config/google-plus.php
+++ config/google-plus.php
@@ -1,5 +1,3 @@
 <?php
 const CLIENT_ID = '399558460038.apps.googleusercontent.com';
 const CLIENT_SECRET = '2Ir8IJ0_bQFmaK1b8AxnXxCX';
-const SCRIPTS = 'https://code.jquery.com/jquery-2.1.0.min.js';
-const STYLES ='http://www.dear-jpn.com/google-plus/styles/html5-doctor-reset-stylesheet.css,http://www.dear-jpn.com/google-plus/styles/main.css';
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "config/google-plus.php";
+include "config/config.php";
 include "classes/Application.php";
 
 $app = new Application();
