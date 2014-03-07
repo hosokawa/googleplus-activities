7942c89d9f40bb772bffe79c4e8a7063e13c697c
HOSOKAWA Tetsuichi <hosokawa@ec-giken.com>
2014-03-07 20:51:34 +0900

    設定ファイルでサーバが変わっても問題ないようにする仕組みを組み込み。

--- config/config.php
+++ config/config.php
@@ -1,3 +1,3 @@
 <?php
 const SCRIPTS = 'https://code.jquery.com/jquery-2.1.0.min.js';
-const STYLES ='http://www.dear-jpn.com/google-plus/styles/html5-doctor-reset-stylesheet.css,http://www.dear-jpn.com/google-plus/styles/main.css';
+const STYLES ='/styles/html5-doctor-reset-stylesheet.css,/styles/main.css';
