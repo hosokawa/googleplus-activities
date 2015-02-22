89f1b510d7b8a9133dce1a72d01fad6691de20b9
hosokawa <hosokawa@ameensoven.com>
2015-02-22 09:30:08 +0300

    on www.dear-jpn.com

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once realpath(__DIR__) . "/classes/Config.php";
-require_once realpath(__DIR__) . "/classes/Application.php";
+require 'vendor/autoload.php';
+require realpath(__DIR__) . "/classes/Config.php";
+require realpath(__DIR__) . "/classes/Application.php";
 
 $config = new GooglePlusAppConfig();
 $app_config = $config->getAppConfig();
-
 $app = new GooglePlusApp($app_config);
-
 $app->run();
